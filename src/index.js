60708a876310b6ca580737a6826aa95d48194c7a
shakir <abdulshakirsam45@gmail.com>
2024-03-26 13:06:19 +0530

    Setup basic dotenv

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+// here we will require the port
+const {PORT}=require('./config/serverConfig');
+const setupAndStartServer= async()=>{
+    // create express object
+    const app=express();
+    // start the app
+    app.listen(PORT,()=>{
+        console.log(`Server started at ${PORT}`);  //here we have done string interpolation
+    });
+}
+setupAndStartServer();
