6f78f633a9b3f87e16af095958a21486fab576ba
shakir <abdulshakirsam45@gmail.com>
2024-03-26 14:34:05 +0530

    Setup basic bodyParser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 // here we will require the port
 const {PORT}=require('./config/serverConfig');
 const setupAndStartServer= async()=>{
     // create express object
     const app=express();
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
     // start the app
     app.listen(PORT,()=>{
       console.log(`Server started at ${PORT}`);  //here we have done string interpolation
